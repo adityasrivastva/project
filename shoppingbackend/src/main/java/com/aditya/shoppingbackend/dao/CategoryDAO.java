a1d6774e289ab352b630c4fcdaea3ceb3dfc65d8
adityasrivastva <33947379+adityasrivastva@users.noreply.github.com>
2017-12-24 17:46:49 +0530

    Create CategoryDAO.java

---
+++ shoppingbackend/src/main/java/com/aditya/shoppingbackend/dao/CategoryDAO.java
@@ -0,0 +1,11 @@
+package com.aditya.shoppingbackend.dao;
+
+import java.util.List;
+
+import com.aditya.shoppingbackend.dto.Category;
+
+public interface CategoryDAO {
+	
+	List<Category> list();
+
+}
