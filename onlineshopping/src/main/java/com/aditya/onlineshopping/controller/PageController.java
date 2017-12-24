5d789538ee19304ffc5db862b5f764327956582a
adityasrivastva <33947379+adityasrivastva@users.noreply.github.com>
2017-12-24 13:01:01 +0530

    Update PageController.java

--- onlineshopping/src/main/java/com/aditya/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/aditya/onlineshopping/controller/PageController.java
@@ -11,11 +11,35 @@ public class PageController {
 	@RequestMapping(value={"/", "/home", "/index"})
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC"); 
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true); 
 		return mv;
 	}
 
-	/*
+	@RequestMapping(value="/about")
+	public ModelAndView about(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true); 
+		return mv;
+	}
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true); 
+		return mv;
+	}
+	
+	
+
+}
+
+
+
+/*
+
 	@RequestMapping(value="/test")
 	public ModelAndView test(@RequestParam(value="greeting", required=false) String greetigs){
 		if (greetigs==null) {
@@ -26,8 +50,7 @@ public class PageController {
 		return mv;
 		
 	}
-	*/
-
+	
 	
 	@RequestMapping(value="/test/{greeting}")
 	public ModelAndView test(@PathVariable("greeting") String greetigs){
@@ -40,6 +63,5 @@ public class PageController {
 		
 	}
 	
-	
 
-}
+*/
