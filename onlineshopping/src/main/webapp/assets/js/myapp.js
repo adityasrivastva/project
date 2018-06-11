308e6b2447e90001317bf8bc5388767534eb0c8f
adityasrivastva <aditya5srivastva@gmail.com>
2018-06-12 01:25:01 +0530

    Code for jquery dataTable for dummy data

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -1,23 +1,52 @@
-$(function(){
-	//solving the active menu problem
-	switch(menu){
-		case 'About Us':
-			$('#about').addClass('active');		//id in navbar as a selector
+$(function() {
+	// solving the active menu problem
+	switch (menu) {
+	case 'About Us':
+		$('#about').addClass('active'); // id in navbar as a selector
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active'); // id in navbar as a selector
+		break;
+	case 'All Products':
+		$('#listProducts').addClass('active'); // id in navbar as a selector
+		break;
+
+	default:
+		if (menu == 'Home')
 			break;
-		case 'Contact Us':
-			$('#contact').addClass('active');		//id in navbar as a selector
-			break;
-		case 'All Products':
-			$('#listProducts').addClass('active');		//id in navbar as a selector
-			break;
+		$('#listProducts').addClass('active');
+		$('#a_' + menu).addClass('active');
+		break;
+	}
+
+	// Code for jquery dataTable
+	// 1. create a dataset
+	var products = [
 		
-		default: 
-			if (menu == 'Home') 
-				break;
-			$('#listProducts').addClass('active');
-			$('#a_'+menu).addClass('active');
-			break;
+						['1', 'ABC'],
+						['2', 'XYZ'],
+						['3', 'PQR'],
+						['4', 'MNO'],
+						['5', 'CFG'],
+						['6', 'HIj'],
+						['7', 'LMP'],
+						['8', 'BLR']
+					];
+	//For jquery element use $table to rap around jquery elements
+	//Fetch table by Id selector of jquery i.e. #
+	var $table = $('#productListTable');
+	
+	//execute the below code only where we have this table
+	// This is only for view products 
+	if ($table.length) {
+		//console.log('Inside the table')
+		
+		$table.DataTable({
+			lengthMenu: [[3,5,10,-1], ['3 Records', '5 Records', '10 Records', 'ALL']],
+			pageLength:5,
+			data: products
+		});
 	}
 	
-	
+
 });
